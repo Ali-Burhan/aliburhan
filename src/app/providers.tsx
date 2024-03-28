0e494392729fb30e0a7ecec635dbb409b8bfb94d
Ali Burhan <aliburhan42000@gmail.com>
2024-03-28 21:10:52 +0500

    Fixed parallax provider

--- src/app/providers.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import { ParallaxProvider } from 'react-scroll-parallax';
-
-export function Providers({ children }: { children: React.ReactNode }) {
-  return <ParallaxProvider>{children}</ParallaxProvider>;
-}
